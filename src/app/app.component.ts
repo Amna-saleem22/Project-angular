b39efe560188762f3eed34fd8d5592fb280b99e5
Amna Saleem <saleemamna431@gmail.com>
2024-12-02 17:51:09 -0700

    api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,24 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'API';
+
+
+  user: any[] = [];
+  constructor( private http : HttpClient){
+    this.getdata();
+  }
+
+   getdata(){
+     this.http.get("https://jsonplaceholder.typicode.com/users").subscribe(
+      (data:any) => {
+        this.user = data
+      },
+      // (error) => {
+      //   console.error('Error fetching users:', error);
+      //   alert('Failed to fetch user data. Please try again later.');
+      // }
+     );
+    
+   }
+
 }
